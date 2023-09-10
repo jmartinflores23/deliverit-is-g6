e3af61bf4508dce2c7ecfd1ae3affd5e0e3f30c6
82454-Carrera-Sebastian <sebaluca.carrera@hotmail.com>
2023-09-10 14:08:44 -0300

    No guarda la info de pestaña forma de pago

--- src/components/Pago.js
+++ src/components/Pago.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react'
-import { Select, Stack, Box, Heading, Flex, Text, Input, Textarea, Radio, RadioGroup } from '@chakra-ui/react';
+import {  Stack, Box, Heading, Flex, Text, Input, Radio, RadioGroup } from '@chakra-ui/react';
 
-function Pago({ formData, setForm }) {
+function Pago({ formData, setFormData }) {
   const [opcionSelecc, setOpcionSelecc] = useState('efectivo');
 
+  // const cambioOpcion = (event) => {
+  //   setOpcionSelecc(event.target.value)
+  // };
+
   const cambioOpcion = (event) => {
-    setOpcionSelecc(event.target.value)
+    const nuevaFormaDePago = event.target.value;
+    setOpcionSelecc(nuevaFormaDePago);
+    // Actualiza formData con la nueva formaDePago
+    setFormData({ ...formData, formaDePago: nuevaFormaDePago });
   };
   return (
 
@@ -14,12 +21,12 @@ function Pago({ formData, setForm }) {
       <Stack isInline justifyContent='center'>
         <Flex direction='column' mr={300 }>
           <label>
-            <input type="radio" value="efectivo" checked={opcionSelecc === 'efectivo'} onChange={cambioOpcion} /> Efectivo
+            <input type="radio" value="efectivo" checked={opcionSelecc === 'efectivo'} onChange={cambioOpcion}/> Efectivo
           </label>
         </Flex>
         <Flex direction='column' ml={300}>
           <label>
-            <input type="radio" value="tarjeta" checked={opcionSelecc === 'tarjeta'} onChange={cambioOpcion} /> Tarjeta
+            <input type="radio" value="tarjeta" checked={opcionSelecc === 'tarjeta'} onChange={cambioOpcion}  /> Tarjeta
           </label>
         </Flex>
       </Stack>
@@ -29,20 +36,24 @@ function Pago({ formData, setForm }) {
       {opcionSelecc === 'efectivo' &&
           <Stack>
           <Text>INDIQUE CON CUANTO VA A ABONAR</Text>
-          <Input variant='filled' htmlsize={5} ></Input>
+          <Input placeholder={formData.cantidadEfectivo || "$ Monto a abonar"} onChange={(e) =>
+          setFormData({ ...formData, cantidadEfectivo: e.target.value })} variant='filled' htmlsize={5} ></Input>
           </Stack>}
       </Stack>
       <Stack p={4} alignItems="center" justifyContent="center">
       {opcionSelecc === 'tarjeta' &&
-        <Stack my='5' textAlign='left' boxShadow='dark-lg' borderWidth={1} borderRadius='lg' p={4}>
+        <Stack  my='5' textAlign='left' boxShadow='dark-lg' borderWidth={1} borderRadius='lg' p={7} widht='full'>
           <Stack isInline>
-            <Text>N° Tarjeta </Text> <Input size='sm' placeholder="0000-0000-0000-0000" type="number" /><br /><br /><br />
+            <Text>N° Tarjeta </Text> <Input size='sm' placeholder={formData.numeroTarjeta || "0000-0000-0000-0000"} 
+            onChange={(e) => setFormData({ ...formData, numeroTarjeta: e.target.value })} type="text" /><br /><br /><br />
           </Stack>
           <Stack isInline >
-              <Text>Caducidad: </Text><Input size='sm' placeholder="MM"  type="number" /> <Input size='sm' htmlsize={4} widht='auto' placeholder="AA" type="number" /><br /><br /><br />
+              <Text>Caducidad: </Text><Input size='sm' placeholder={formData.fechaVen || "MM/AA"} onChange={(e) =>
+          setFormData({ ...formData, fechaVen: e.target.value })} type="text" /> <br /><br /><br />
           </Stack>
           <Stack>
-              <Text>CÓD SEGURIDAD</Text> <Input placeholder="000" type="number" /><br />
+              <Text>CÓD SEGURIDAD</Text> <Input placeholder={formData.codSeg || "000"} onChange={(e) =>
+          setFormData({ ...formData, codSeg: e.target.value })} type="number" /><br />
           </Stack>
          
         </Stack>}
