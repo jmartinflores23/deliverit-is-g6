9dfeadac2fd01c4f61df158f041a60eb3daca37f
JuaniMolina48 <juanirojo10@gmail.com>
2023-09-10 20:04:23 -0300

    Titulo e Icono

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
 import Formulario from './components/Formulario';
+import { Heading, Box, Icon } from '@chakra-ui/react';
+import { GiKnifeFork } from 'react-icons/gi'
 
 
 
 function App() {
   return (
     <div className="App">
-      
-      <h2>Forma Pago</h2>
+      <Box bg='lightgray' w='100%' p={4} color='black' marginBottom='20' /* borderWidth='0.5em' borderColor='black' */>
+        <Icon as={GiKnifeFork} boxSize={12} />
+        <Heading display='inline' size='4xl' fontFamily='Merriwheater'>  DeliverIT!</Heading>
+
+      </Box>
       <Formulario/>
     </div>
   );
