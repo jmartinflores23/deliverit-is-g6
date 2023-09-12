5c0972deffef4ccd9c99924fd76f4dd16c77b863
Guty04 <86425@sistemas.frc.utn.edu.ar>
2023-09-12 11:31:10 -0300

    ultimo retoques

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { GiKnifeFork } from 'react-icons/gi'
 function App() {
   return (
     <div className="App">
-      <Box bg='lightgray' w='100%' p={4} color='black' marginBottom='20' /* borderWidth='0.5em' borderColor='black' */>
+      <Box bg='#007090' w='100%' p={4} color='white' marginBottom='20' /* borderWidth='0.5em' borderColor='black' */>
         <Icon as={GiKnifeFork} boxSize={12} />
         <Heading display='inline' size='3xl' fontFamily='Merriwheater'>  DeliverEat!</Heading>
       </Box>
